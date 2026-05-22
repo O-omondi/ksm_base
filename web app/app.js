dc810bb54bda21b8c1f467dada29513ec29c27a3
Omondi <114482540+O-omondi@users.noreply.github.com>
2026-05-22 15:22:23 +0300

    Add files via upload

---
+++ web app/app.js
@@ -0,0 +1,202 @@
+// 1. Initialize the map
+var map = L.map('map').setView([-0.0917, 34.7680], 10);
+
+// 2. Add OpenStreetMap base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// 3. Load county boundary
+fetch('app_data/county_boundary.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#ff0000',
+                weight: 3,
+                fillColor: '#ff0000',
+                fillOpacity: 0.1
+            }
+        }).addTo(map);
+    });
+    // 4. Load subcounties
+fetch('app_data/sub_counties.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#0000ff',
+                weight: 2,
+                fillColor: '#0000ff',
+                fillOpacity: 0.1
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Subcounty: </b>' + feature.properties.Name
+                );
+            }
+        }).addTo(map);
+    });
+    // 5. Load wards
+fetch('app_data/ksm_wards.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#008000',
+                weight: 1,
+                fillColor: '#008000',
+                fillOpacity: 0.1
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Ward: </b>' + feature.properties.Kenya_Ward
+                );
+            }
+        }).addTo(map);
+    });
+        // 7. Load parks
+fetch('app_data/parks.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#006600',
+                weight: 1,
+                fillColor: '#90EE90',
+                fillOpacity: 0.5
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Park: </b>' + feature.properties.NAME
+                );
+            }
+        }).addTo(map);
+    });
+    // 6. Load rivers
+fetch('app_data/rivers.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#0066cc',
+                weight: 1.5,
+            }
+        }).addTo(map);
+    });
+    // 8. Load SGR
+fetch('app_data/sgr.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: {
+                color: '#8B4513',
+                weight: 2,
+            }
+        }).addTo(map);
+    });
+    // 9. Load towns
+fetch('app_data/towns.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 6,
+                    fillColor: '#ff7800',
+                    color: '#000',
+                    weight: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Town: </b>' + feature.properties.TName
+                );
+            }
+        }).addTo(map);
+    });
+    // 10. Load primary schools
+fetch('app_data/primary_schools.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 5,
+                    fillColor: '#ffff00',
+                    color: '#000',
+                    weight: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Primary School: </b>' + feature.properties.NAME
+                );
+            }
+        }).addTo(map);
+    });
+    // 11. Load secondary schools
+fetch('app_data/secondary_schools.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 5,
+                    fillColor: '#ff0000',
+                    color: '#000',
+                    weight: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Secondary School: </b>' + feature.properties.NAME
+                );
+            }
+        }).addTo(map);
+    });
+    // 12. Load colleges
+fetch('app_data/collages.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 5,
+                    fillColor: '#800080',
+                    color: '#000',
+                    weight: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>College: </b>' + feature.properties.NAME_OF_SC
+                );
+            }
+        }).addTo(map);
+    });
+    // 13. Load facilities
+fetch('app_data/facilities.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 5,
+                    fillColor: '#00ffff',
+                    color: '#000',
+                    weight: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(
+                    '<b>Facility: </b>' + feature.properties.Name
+                );
+            }
+        }).addTo(map);
+    });
